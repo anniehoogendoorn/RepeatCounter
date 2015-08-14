4307f455aafe716294f9dde8558096c2b9f6930d
Annie <anniehoogendoorn@gmail.com>
2015-08-14 12:41:42 -0700

    Add fifth test for capital letters used in word.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,6 +4,8 @@
     {
         function countRepeats($word, $string)
         {
+            $word = strtolower($word);
+            $string = strtolower($string);
             $repeats = substr_count($string, $word);
             return $repeats;
 
