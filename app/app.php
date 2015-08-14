f7d5ef1ba67500886d7e0c872836a4a431cfbbfb
Annie <anniehoogendoorn@gmail.com>
2015-08-14 15:07:58 -0700

    Add app/app.php file.

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/RepeatCounter.php';
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    //Home page twig link
+    $app->get("/", function() use ($app)
+    {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    //Results page twig link
+    $app->post("/results", function() use ($app) {
+        $new_RepeatCounter = new RepeatCounter;
+        $results = $new_RepeatCounter->countRepeats($_POST['input']);
+        return $app['twig']->render('results.html.twig', array('result' => $results));
+    });
+    
+    return $app;
+?>
